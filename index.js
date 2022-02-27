5339b8d3eedf3c712537bab74c3e4b95f1ccd55a
Diego Michalski <michalski.diego@gmail.com>
2022-02-26 23:45:14 -0300

    Initial commit
    Font, JS, CSS and background image

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    // Set remaining days
+    const days = Math.ceil((new Date("2022-06-21").getTime() - (new Date()).getTime()) / (1000 * 3600 * 24)) + 1;
+    document.querySelector('h1').textContent = document.querySelector('h1').textContent + days;
+
+    // Resize body
+    //document.querySelector('body').style.height = window.innerHeight;
+})
